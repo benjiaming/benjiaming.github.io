45da829df636499f4c5fd2be867475374dfefc7f
benjiaming <benjiaming@users.noreply.github.com>
2025-06-08 21:56:12 -0700

    fix test-ltl

--- zhongwen/test/cypress.config.js
+++ zhongwen/test/cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:1234',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-})
+});
